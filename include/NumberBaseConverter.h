0ecd89e057ee2af93ec91d427646ffb3cce189b7
Samiul946 <164762138+Samiul946@users.noreply.github.com>
2025-08-23 15:03:42 +0600

    Update NumberBaseConverter.h

--- include/NumberBaseConverter.h
+++ include/NumberBaseConverter.h
@@ -18,6 +18,6 @@ bool is_valid_input(const char* input, int base);
 bool from_any_to_dec(const char* input, int base, long long* out_val);
 char* from_dec_to_any(long long val, int base);
 char* convert_base(const char* input, int src_base, int dst_base);
-int get_numeric_base_input(const char* prompt);
+static char* get_dynamic_input(const char* prompt);
 
 #endif // NUMBER_BASE_CONVERTER_H
